564edf4ce42f87f15873ad2d1ca8c6331efc7fad
tong.luo <luotong@leyou.com>
2020-04-14 13:35:54 +0800

    add

--- gateway/src/main/java/com/lt/gateway/use/Cotroller/TestComtroller.java
+++
@@ -1,7 +0,0 @@
-package com.lt.gateway.use.Cotroller;
-
-/**
- * Created by tong.luo on 2019/10/20 20:20
- */
-public class TestComtroller {
-}
--- gateway/src/main/java/com/lt/gateway/use/GateApplicationAop.java
+++
@@ -1,7 +0,0 @@
-package com.lt.gateway.use;
-
-/**
- * Created by tong.luo on 2019/8/6 23:32
- */
-public class GateApplicationAop {
-}
