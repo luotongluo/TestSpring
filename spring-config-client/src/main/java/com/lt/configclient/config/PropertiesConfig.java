a95e44485669a0069185e5c58886c3f98bc81934
tong.luo <luotong@dazhangfang.com>
2020-12-30 18:03:34 +0800

    add springcloud config add files

---
+++ spring-config-client/src/main/java/com/lt/configclient/config/PropertiesConfig.java
@@ -0,0 +1,24 @@
+//package com.lt.configclient.config;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.cloud.context.config.annotation.RefreshScope;
+//import org.springframework.context.annotation.Bean;
+//
+///**
+// * @author tong.luo
+// * @description PropertiesConfig
+// * @date 2020/12/30 17:58
+// */
+//@RefreshScope
+//public class PropertiesConfig {
+//    @Value("${springcloud.hello}")
+//    private String hello;
+//
+//    public String getHello() {
+//        return hello;
+//    }
+//
+//    public void setHello(String hello) {
+//        this.hello = hello;
+//    }
+//}
--- spring-config-client/src/main/java/com/lt/configclient/controller/HelloController.java
+++ spring-config-client/src/main/java/com/lt/configclient/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.lt.configclient.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.helpers.SubstituteLogger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,15 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
  * @description Controller
  * @date 2020/12/30 15:24
  */
-@RestController
 @RefreshScope
+@RestController
 public class HelloController {
     private Logger logger = LoggerFactory.getLogger(HelloController.class);
     @Value("${springcloud.hello}")
     private String hello;
+//    @Autowired
+//    private PropertiesConfig propertiesConfig;
 
     @RequestMapping("/hello")
     public String from() {
-        return this.hello;
+        return hello;
     }
 }
