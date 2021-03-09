cc4f23d30c0d00a7a15e2bbdded56e3fa651351b
tong.luo <luotong@dazhangfang.com>
2021-03-09 17:42:28 +0800

    add files

--- con-con/src/main/java/com/lt/concon/cofig/MqContents.java
+++ con-con/src/main/java/com/lt/concon/cofig/MqContents.java
@@ -6,8 +6,6 @@ package com.lt.concon.cofig;
  * @date 2020/4/20 22:45
  */
 public class MqContents {
-
-
     public final static String LT_TEST_QUEUE = "lt.test.queue";
     public final static String LT_TEST_QUEUE_KEY = "lt.test.queue.key";
     public final static String LT_TEST_EXHCANGE = "lt.test.exchange";
